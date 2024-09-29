a40b5a327a7108ad341df3c58eedf673b14414a8
Duvan <duvanriano2626@gmail.com>
2024-09-29 11:54:58 -0500

    Obtener un carro por id

--- src/cars/cars.controller.ts
+++ src/cars/cars.controller.ts
@@ -1,10 +1,24 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
+
 
 @Controller('cars')
 export class CarsController {
 
+    private cars = ['Toyota','Honda','Jeep'];
+
     @Get()
     public getAllCars(){
-        return ['Toyota','Honda','Jeep'];
+        return this.cars;
+    }
+
+    
+    @Get(':id')
+    public getCarById(@Param('id') id:string) {
+        let car = this.cars[+id]
+        console.log({id:car});
+
+        return {
+            car
+        }
     }
 }
