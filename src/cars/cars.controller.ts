8332b2d8cfe5385fa664296ce1c117ca3d792c4b
Duvan <duvanriano2626@gmail.com>
2024-09-29 11:02:03 -0500

    Se aprende sobre el controlador get

---
+++ src/cars/cars.controller.ts
@@ -0,0 +1,10 @@
+import { Controller, Get } from '@nestjs/common';
+
+@Controller('cars')
+export class CarsController {
+
+    @Get()
+    public getAllCars(){
+        return ['Toyota','Honda','Jeep'];
+    }
+}
